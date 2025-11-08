b850accf88e7d2f735c59d6302dfcf661bee1569
Hasan <darkhasan05@protonmail.com>
2025-11-08 12:45:15 +0330

    refactor(models): use  and remove  in models
    Replace  with  in models since all fields are fillable, simplifying model configuration.

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,23 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'post_id',
-        'user_id',
-        'body',
-    ];
+    protected $guarded = [];
 
-    public function post()
+    public function post(): BelongsTo
     {
         return $this->belongsTo(Post::class);
     }
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,23 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
     use HasFactory;
-    
-    protected $fillable = [
-        'user_id',
-        'title',
-        'body',
-    ];
 
-    public function user()
+    protected $guarded = [];
+
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function comments()
+    public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
     }
